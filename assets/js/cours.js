6cf8319fe40e559226b52ce0df69d2e7562baa91
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-21 10:40:18 +0200

    ajout de la protection de saisie

--- assets/js/cours.js
+++ assets/js/cours.js
@@ -1,9 +1,13 @@
 function number(){
-  var Age= document.getElementById("age").value;
-  if(Age>=18){
-    alert("Vous êtes majeur");
-  }
-  else{
-    alert("Vous êtes mineur");
+  var age = document.getElementById("age").value;
+  var regex = /^[\d]{2}$/;
+  if(regex.test(age) == true){
+      if(age>=18){
+        alert("Vous êtes majeur");
+      }else{
+        alert("Vous êtes mineur");
+      }
+  }else{
+    alert("Insérez un nombre à deux chiffres pour indiquer votre âge !");
   }
 }
