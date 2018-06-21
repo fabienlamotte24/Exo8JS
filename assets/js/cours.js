a105c01d98e05518d9a6e7eb0a77af2483335cae
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-21 15:09:22 +0200

    mise à jour apres correction

--- assets/js/cours.js
+++ assets/js/cours.js
@@ -1,6 +1,6 @@
 function number(){
   var age = document.getElementById("age").value;
-  var regex = /^[\d]{2}$/;
+  var regex = /^[\d]{1,3}$/;
   if(regex.test(age) == true){
       if(age>=18){
         alert("Vous êtes majeur");
@@ -8,6 +8,6 @@ function number(){
         alert("Vous êtes mineur");
       }
   }else{
-    alert("Insérez un nombre à deux chiffres pour indiquer votre âge !");
+    alert("Vous n'avez le droit qu'à la saisie de chiffre!");
   }
 }
